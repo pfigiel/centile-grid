3cc545ac8f7dec80d4e6bdcedb68e5f3dba4bb71
Paweł Figiel <pawel.figiel8@gmail.com>
2026-05-04 13:43:21 +0200

    test(ui-kit-mobile): add Skia Jest mock

--- packages/ui-kit-mobile/jest.config.mjs
+++ packages/ui-kit-mobile/jest.config.mjs
@@ -1,4 +1,7 @@
 export default {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  moduleNameMapper: {
+    '^@shopify/react-native-skia$': '@shopify/react-native-skia/src/mock',
+  },
 };
