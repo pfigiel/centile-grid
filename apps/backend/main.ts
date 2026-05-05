4014d062828e8bb235af958d0784992ed9dda056
Paweł Figiel <pawel.figiel8@gmail.com>
2026-05-05 12:01:37 +0200

    feat(backend): add local HTTP server entrypoint

---
+++ apps/backend/main.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './src/app.module';
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+};
+
+bootstrap();
