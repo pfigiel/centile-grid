2d98e47399e637314103ac91839b09f0a14d9660
Paweł Figiel <pawel.figiel8@gmail.com>
2026-05-11 19:21:21 +0200

    feat: adjust styling of centile value section

--- apps/mobile/app/results/index.tsx
+++ apps/mobile/app/results/index.tsx
@@ -1,18 +1,11 @@
-import { getCentileLabel } from '@/utils/getCentileLabel/getCentileLabel'
 import { useChartsData } from '@/hooks/useChartsData'
 import { GrowthParameter } from '@/types'
-import { LineChart } from '@centile-grid/ui-kit-mobile'
+import { getCentileLabel } from '@/utils/getCentileLabel/getCentileLabel'
+import { Button, LineChart } from '@centile-grid/ui-kit-mobile'
 import { router, useLocalSearchParams } from 'expo-router'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import {
-  ActivityIndicator,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native'
+import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 type Params = {
   gender: string
@@ -49,16 +42,14 @@ const ResultsScreen = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
-        <Text>{t('results.back')}</Text>
-      </TouchableOpacity>
+      <Button onPress={router.back}>{t('results.back')}</Button>
       <View style={styles.centileSection}>
         {chartData?.map((data, index) => (
           <View key={index} style={styles.centileRow}>
-            <Text>
+            <Text style={styles.parameterLabel}>
               {data.parameter === 'height' ? t('results.heightLabel') : t('results.weightLabel')}
             </Text>
-            <Text>
+            <Text style={styles.centileValue}>
               {getCentileLabel(
                 data.lineSeries.map((ls) => ls.data),
                 Number(age),
@@ -96,12 +87,18 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     gap: 16,
   },
-  backButton: {
-    paddingVertical: 8,
-  },
   centileSection: {
+    padding: 8,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.1)',
     gap: 4,
   },
+  parameterLabel: {
+    fontWeight: 500,
+    minWidth: 50,
+  },
+  centileValue: {},
   centileRow: {
     flexDirection: 'row',
     gap: 16,
